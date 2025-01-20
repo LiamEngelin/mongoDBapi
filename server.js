7fbdb3a4f47c560e7ee69015b1a32ed448830a7c
LiamEngelin <liam.engelin@iths.se>
2025-01-20 15:15:44 +0100

    added all crud

--- server.js
+++ server.js
@@ -24,6 +24,53 @@ app.post("/api/product", async (req, res) =>{
     }
 });
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.get("/api/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        res.status(200).json(product);
+
+        if(!product){
+            return res.status(404).json({message: "Product does not exist"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.delete("/api/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id)
+        res.status(200).json(product);
+
+        if(!product){
+            res.status(404).json({message: "Product does not exist"})
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 mongoose.
 connect(process.env.MONGO_URI)
 .then(() => {
