439281983a16ff83af46aaf91d9ca2e7526c87d1
LiamEngelin <liam.engelin@iths.se>
2025-01-20 10:38:42 +0100

    Added boilerplate code for server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+const express = require("express")
+const app = express();
+const PORT = 8080;
+
+app.listen(PORT,(req, res) => {
+    console.log(`Live på http://localhost:${PORT}`)
+})
