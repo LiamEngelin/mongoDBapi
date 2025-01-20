bd4d5beeb329e7f6824c87be9e4f21453766da43
LiamEngelin <liam.engelin@iths.se>
2025-01-20 11:47:47 +0100

    linked api to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express")
 const app = express();
 const PORT = 8080;
+const mongoose = require("mongoose")
 
-app.listen(PORT,(req, res) => {
-    console.log(`Live på http://localhost:${PORT}`)
-})
+
+app.get("/",(req,res) => {
+    res.send("Welcome to my mongoAPI")
+});
+
+app.post("/blogpost", (req,res) => {
+    res.send("This is a postrequest")
+});
+
+mongoose.
+connect("mongodb+srv://liamiths2024:liamiths2024@mycluster.5gnyu.mongodb.net/Products-API?retryWrites=true&w=majority&appName=MyCluster")
+.then(() => {
+    console.log("Connected to mongoDatabase");
+    app.listen(PORT,(req, res) => {
+        console.log(`Live på http://localhost:${PORT}`)
+    });
+});
